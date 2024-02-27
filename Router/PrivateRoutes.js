7c4fbe8849866bc4cd355a1ff746c4e8e4999a50
Darshan Vaghasiya <darshan.vaghasiya@nexuslinkservices.in>
2024-02-27 13:11:47 +0530

    add router

---
+++ Router/PrivateRoutes.js
@@ -0,0 +1,34 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+const PrivateRoutes = () => {
+  let auth = { token: false };
+  return auth.token ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoutes;
+
+
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Login from './pages/Login'
+import PrivateRoutes from './utils/PrivateRoutes'
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+          <Routes>
+            <Route element={<PrivateRoutes />}>
+                <Route element={<Home/>} path="/" exact/>
+                <Route element={<Products/>} path="/products"/>
+            </Route>
+            <Route element={<Login/>} path="/login"/>
+          </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
